3852df13317b155d309cbc711347504e27c4ef0a
Guilherme <g.martiins.silva15@gmail.com>
2025-07-17 21:47:56 -0400

    Configuring the Spring connection to Docker with creating Models

---
+++ vacation-backend/src/test/java/com/rh/vacationbackend/VacationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.vacationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
